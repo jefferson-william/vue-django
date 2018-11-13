b38fae6b11000b132429e844abcf3b669238911c
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2018-11-13 13:46:46 -0200

    vue-django with apps folder

---
+++ apps/chat/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from app.views import index
+
+class TestView(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_index_view(self):
+        response = self.client.get('/chat')
+        self.assertEqual(response.status_code, 200)
---
+++ vuedj/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from app.views import index
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='home'),
+]
